b1b9cbcfd40baf99530afecbdb2366e5b21bbc99
Nicolas Fabre <nfabredev@gmail.com>
2023-01-10 09:27:51 +0200

    Stage 1: Bind to a port

--- app/server.go
+++ app/server.go
@@ -1,26 +1,36 @@
+// server.go
+
 package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"net"
+	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	startServer()
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	os.Exit(0)
+}
+
+func startServer() {
+	fmt.Println("Starting to listen")
+
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	fmt.Println("Listening")
+
+	conn, err := listener.Accept()
+	defer conn.Close()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Connection accepted")
+
+	return 
 }
---
+++ app/server_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net"
+	"testing"
+	"time"
+	"fmt"
+)
+
+func TestStartServer(t *testing.T) {
+	go startServer()
+	
+	retries := 0
+	
+	for {
+		_, err := net.Dial("tcp", "0.0.0.0:6379")
+
+		if err != nil && retries > 10 {
+			t.Fatal("All retries failed.")
+		}
+
+		if err != nil {
+			fmt.Println("Failed to connect to port 6379, retrying in 1s")
+
+			retries += 1
+			time.Sleep(1 * time.Second)
+		} else {
+			break
+		}
+	}
+
+	return
+}
